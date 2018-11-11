ec4429370c510039258f34afaf2aed919f8ccfb7
BlackRock404 <blackrock4044@gmail.com>
2018-11-11 16:22:20 +0300

    about btn && toolbar

---
+++ app/src/main/java/com/enchantme/akali/AboutActivity.java
@@ -0,0 +1,107 @@
+package com.enchantme.akali;
+
+import androidx.appcompat.app.AlertDialog;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
+import androidx.core.content.ContextCompat;
+
+import android.Manifest;
+import android.content.Context;
+import android.content.DialogInterface;
+import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.net.Uri;
+import android.os.Bundle;
+import android.provider.Settings;
+import android.telephony.TelephonyManager;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+import android.widget.Toast;
+
+public class AboutActivity extends AppCompatActivity {
+
+    private TextView versionTextView;
+    private TextView IMEITextView;
+    private Button settingsButton;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_about);
+
+        versionTextView = findViewById(R.id.version_textview);
+        IMEITextView = findViewById(R.id.imei_textview);
+        settingsButton = findViewById(R.id.get_permission_button);
+
+        View.OnClickListener getPermission = new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                Intent intent = new Intent();
+                intent.setAction(Settings.ACTION_APPLICATION_DETAILS_SETTINGS);
+                Uri uri = Uri.fromParts("package", getPackageName(), null);
+                intent.setData(uri);
+                startActivity(intent);
+            }
+        };
+
+        settingsButton.setOnClickListener(getPermission);
+
+        setVersionTextView();
+        if (savedInstanceState != null) {
+            IMEITextView.setText(savedInstanceState.getCharSequence("IMEI"));
+        } else {
+            setIMEI();
+        }
+    }
+
+    private void setVersionTextView() {
+        String versionName = BuildConfig.VERSION_NAME;
+
+        versionTextView.setText(versionName);
+    }
+
+    @Override
+    public void onSaveInstanceState(Bundle outState) {
+        outState.putCharSequence("IMEI", IMEITextView.getText());
+        super.onSaveInstanceState(outState);
+    }
+
+    private void setIMEI() {
+        if (ContextCompat.checkSelfPermission(this, Manifest.permission.READ_PHONE_STATE)
+                != PackageManager.PERMISSION_GRANTED) {
+
+            if(ActivityCompat.shouldShowRequestPermissionRationale(this,
+                    Manifest.permission.READ_PHONE_STATE)) {
+
+                AlertDialog.Builder builder=new AlertDialog.Builder(this);
+                builder.setTitle(R.string.imei_dialog_title)
+                        .setMessage(R.string.imei_dialog_message)
+                        .setPositiveButton(R.string.ok, new DialogInterface.OnClickListener() {
+                            public void onClick(DialogInterface dialog, int id) {
+                                requestPermission();
+                            }
+                        })
+                        .setNegativeButton(R.string.cancel,null)
+                        .create()
+                        .show();
+            } else {
+                requestPermission();
+            }
+
+        } else {
+            String IMEINumber;
+            TelephonyManager telephonyManager = (TelephonyManager)getSystemService(Context.TELEPHONY_SERVICE);
+
+            IMEINumber = telephonyManager.getDeviceId();
+            IMEITextView.setText(IMEINumber);
+        }
+    }
+
+    private void requestPermission(){
+        ActivityCompat.requestPermissions(this,
+                new String[]{Manifest.permission.READ_PHONE_STATE},
+                Constants.REQUEST_FOR_IMEI);
+    }
+}
--- app/src/main/java/com/enchantme/akali/MainActivity.java
+++ app/src/main/java/com/enchantme/akali/MainActivity.java
@@ -6,101 +6,51 @@ import android.content.Intent;
 import android.content.pm.PackageManager;
 import android.net.Uri;
 import android.provider.Settings;
+
+import androidx.appcompat.widget.Toolbar;
 import androidx.core.app.ActivityCompat;
 import androidx.core.content.ContextCompat;
 import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 import android.telephony.TelephonyManager;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import static android.Manifest.*;
 
 public class MainActivity extends AppCompatActivity {
 
-    private TextView versionTextView;
-    private TextView IMEITextView;
-    private Button settingsButton;
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
-        versionTextView = findViewById(R.id.version_textview);
-        IMEITextView = findViewById(R.id.imei_textview);
-        settingsButton = findViewById(R.id.get_permission_button);
-
-        View.OnClickListener getPermission = new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-
-                Intent intent = new Intent();
-                intent.setAction(Settings.ACTION_APPLICATION_DETAILS_SETTINGS);
-                Uri uri = Uri.fromParts("package", getPackageName(), null);
-                intent.setData(uri);
-                startActivity(intent);
-            }
-        };
-
-        settingsButton.setOnClickListener(getPermission);
-
-        setVersionTextView();
-        if (savedInstanceState != null) {
-            IMEITextView.setText(savedInstanceState.getCharSequence("IMEI"));
-        } else {
-            setIMEI();
-        }
+        Toolbar mainToolbar = findViewById(R.id.main_toolbar);
+        setSupportActionBar(mainToolbar);
     }
 
-    private void setVersionTextView() {
-        String versionName = BuildConfig.VERSION_NAME;
 
-        versionTextView.setText(versionName);
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater menuInflater = getMenuInflater();
+        menuInflater.inflate(R.menu.appbar_menu, menu);
+        return true;
     }
 
     @Override
-    public void onSaveInstanceState(Bundle outState) {
-        outState.putCharSequence("IMEI", IMEITextView.getText());
-        super.onSaveInstanceState(outState);
-    }
+    public boolean onOptionsItemSelected(MenuItem item) {
 
-        private void setIMEI() {
-        if (ContextCompat.checkSelfPermission(this, permission.READ_PHONE_STATE)
-                != PackageManager.PERMISSION_GRANTED) {
-
-            if(ActivityCompat.shouldShowRequestPermissionRationale(this,
-                    permission.READ_PHONE_STATE)) {
-
-                AlertDialog.Builder builder=new AlertDialog.Builder(this);
-                builder.setTitle(R.string.imei_dialog_title)
-                        .setMessage(R.string.imei_dialog_message)
-                        .setPositiveButton(R.string.ok, new DialogInterface.OnClickListener() {
-                            public void onClick(DialogInterface dialog, int id) {
-                                requestPermission();
-                            }
-                        })
-                        .setNegativeButton(R.string.cancel,null)
-                        .create()
-                        .show();
-            } else {
-                requestPermission();
-            }
-
-        } else {
-            String IMEINumber;
-            TelephonyManager telephonyManager = (TelephonyManager)getSystemService(Context.TELEPHONY_SERVICE);
-
-            IMEINumber = telephonyManager.getDeviceId();
-            IMEITextView.setText(IMEINumber);
+        switch(item.getItemId()){
+            case R.id.about_menu_item:
+                Intent intent = new Intent(this, AboutActivity.class);
+                startActivity(intent);
+                break;
         }
-    }
-
-    private void requestPermission(){
-        ActivityCompat.requestPermissions(this,
-                new String[]{permission.READ_PHONE_STATE},
-                Constants.REQUEST_FOR_IMEI);
+        return true;
     }
 }
